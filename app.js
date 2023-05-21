36d19b0e1b96b489fa5ad475ccb5cce7c9824998
chandansgowda <chandansuresh007@gmail.com>
2023-05-21 23:17:48 +0530

    feat: get ledStatus from firebase

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 const port = 3000;
 const app = express();
+const firebaseapp = require('./config')
+const { getDatabase, ref, child, get } = require("firebase/database");
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.end("Hello from server")
+app.get("/home1", async (req, res)=> {
+  const dbRef = ref(getDatabase());
+  let hall = await (await get(child(dbRef, `home1/hall`))).val()
+  let room = await (await get(child(dbRef, `home1/room`))).val()
+  let kitchen = await (await get(child(dbRef, `home1/kitchen`))).val()
+
+  res.json({
+    'room':room,
+    'hall': hall,
+    'kitchen': kitchen
   });
 
+});
+
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
+  console.log(`Server is listening on port ${port}`);
+});
