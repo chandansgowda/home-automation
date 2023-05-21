2089e11d2b6eb49117d228cf4f54b7b5985b528b
chandansgowda <chandansuresh007@gmail.com>
2023-05-21 23:43:13 +0530

    feat: change ledStatus post endpoint

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const port = 3000;
 const app = express();
-const firebaseapp = require('./config')
-const { getDatabase, ref, child, get } = require("firebase/database");
+const firebaseapp = require("./config");
+const { getDatabase, ref, child, get, set } = require("firebase/database");
 
 app.use(express.json());
 
-app.get("/home1", async (req, res)=> {
+app.get("/home1", async (req, res) => {
   const dbRef = ref(getDatabase());
-  let hall = await (await get(child(dbRef, `home1/hall`))).val()
-  let room = await (await get(child(dbRef, `home1/room`))).val()
-  let kitchen = await (await get(child(dbRef, `home1/kitchen`))).val()
+  let hall = await (await get(child(dbRef, `home1/hall`))).val();
+  let room = await (await get(child(dbRef, `home1/room`))).val();
+  let kitchen = await (await get(child(dbRef, `home1/kitchen`))).val();
 
   res.json({
-    'room':room,
-    'hall': hall,
-    'kitchen': kitchen
+    room: room,
+    hall: hall,
+    kitchen: kitchen,
   });
+});
 
+//JSON request example - {"key":"hall", "value":true}
+app.post("/home1", async (req, res) => {
+  try {
+    const db = getDatabase();
+    const { key, value } = req.body;
+
+    await set(ref(db,`home1/${key}`), value);
+
+    res.json({ success: true });
+
+  } catch (error) {
+    console.error("Error writing data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {
